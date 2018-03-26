398de500d774dba86f927cc2170848265fdec3cb
Maxai <maxali946@gmail.com>
2018-03-26 11:53:38 +0400

    Add files via upload
    75%

--- PhotoGallery/PhotoGallery/MainWindow.xaml.cs
+++ PhotoGallery/PhotoGallery/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ using System.Windows.Controls;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Windows.Forms;
+
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System;
@@ -17,11 +17,11 @@ namespace PhotoGallery
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Picture> pictures { get; set; }
+        public ObservableCollection<ImageSource> pictures { get; set; }
 
         System.Windows.Forms.SaveFileDialog saveFileDialog;
         System.Windows.Forms.OpenFileDialog openFileDialog;
-        FolderBrowserDialog folderDlg;
+        System.Windows.Forms.FolderBrowserDialog folderDlg;
         //--------------------------------------------
         public MainWindow()
         {
@@ -29,7 +29,7 @@ namespace PhotoGallery
 
             DataContext = this;
 
-            pictures = new ObservableCollection<Picture>();
+            pictures = new ObservableCollection<ImageSource>();
 
             saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             saveFileDialog.Filter = "Picture file (.jpg, .png)|*.jpg; *.png|*.png|*.png|*.jpg|*.jpg";
@@ -37,36 +37,54 @@ namespace PhotoGallery
             openFileDialog = new System.Windows.Forms.OpenFileDialog();
             openFileDialog.Filter = "Picture file (.jpg, .png)|*.jpg; *.png|*.png|*.png|*.jpg|*.jpg";
 
-            folderDlg = new FolderBrowserDialog();
+            folderDlg = new System.Windows.Forms.FolderBrowserDialog();
             folderDlg.ShowNewFolderButton = false;
+
+
+            var lb = new ListBox();
+            lb.ItemTemplate = this.Resources["smallIconsTemplate"] as DataTemplate;
+            lb.ItemsPanel = this.Resources["smaillIconsPanel"] as ItemsPanelTemplate;
         }
         //--------------------------------------------
-        void Save()
+        void Save(bool save)
         {
-            StringBuilder path = new StringBuilder();
-
-            folderDlg.ShowNewFolderButton = true;
-
-            if (folderDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (save)
             {
-                path.Clear();
-
-                path.Append(folderDlg.SelectedPath);
-
                 foreach (var item in pictures)
                 {
-                    File.Copy(item.picSource.ToString(), path.ToString() + item.picSource.ToString().Substring(item.picSource.ToString().LastIndexOf('/')), true);
+                    string temp = Path.GetFullPath(item.ToString().Substring(8));
+
+                    File.Copy(temp, temp, true);
                 }
             }
+            else
+            {
+                string path;
 
-            folderDlg.ShowNewFolderButton = false;
+                folderDlg.ShowNewFolderButton = true;
+
+                if (folderDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    path = folderDlg.SelectedPath;
+
+                    foreach (var item in pictures)
+                    {
+                        string temp = Path.GetFullPath(item.ToString().Substring(8));
+
+                        File.Copy(temp, Path.Combine(path.ToString(), Path.GetFileName(temp)), true);
+                    }
+                }
+
+                folderDlg.ShowNewFolderButton = false;
+            }
         }
         //--------------------------------------------
         private void MenuNew_Click(object sender, RoutedEventArgs e)
         {
             if (pictures.Count != 0)
             {
-                Save();
+                Save(false);
+                pictures.Clear();
             }
         }
         //--------------------------------------------
@@ -74,10 +92,9 @@ namespace PhotoGallery
         {
             if (pictures.Count != 0)
             {
-                Save();
+                Save(false);
                 pictures.Clear();
             }
-
 
             if (folderDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
@@ -92,7 +109,7 @@ namespace PhotoGallery
                             picSource = new BitmapImage(new Uri(pic.FullName))
                         };
 
-                        pictures.Add(picture);
+                        pictures.Add(new BitmapImage(new Uri(pic.FullName)));
                     }
                 }
             }
@@ -102,7 +119,7 @@ namespace PhotoGallery
         {
             if (pictures.Count != 0)
             {
-                Save();
+                Save(true);
             }
         }
         //--------------------------------------------
@@ -110,7 +127,7 @@ namespace PhotoGallery
         {
             if (pictures.Count != 0)
             {
-                Save();
+                Save(false);
             }
         }
         //--------------------------------------------
@@ -123,7 +140,7 @@ namespace PhotoGallery
                 switch (ans)
                 {
                     case MessageBoxResult.Yes:
-                        Save();
+                        Save(false);
                         return;
                     case MessageBoxResult.No:
                         break;
@@ -136,15 +153,14 @@ namespace PhotoGallery
             }
             else
                 PhotoGalleryWindow.Close();
-
         }
         //--------------------------------------------
         private void Button_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var img = e.OriginalSource as Image;
+            var img = sender as System.Windows.Controls.Button;
 
             var pWindow = new Preview { Owner = this };
-            pWindow.PreviewImage.Source = img.Source;
+            pWindow.PreviewImage.Source = img.Tag as ImageSource;
             pWindow.Show();
         }
         //--------------------------------------------
